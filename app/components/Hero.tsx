724a99272652b9a51a7bc67c86c5965fe07b3be9
dani-szoverfi-prisma <223485093+dani-szoverfi-prisma@users.noreply.github.com>
2026-04-10 09:30:46 +0300

    init commit and header done

---
+++ app/components/Hero.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Hero = () => {
+	return (
+		<div className="min-h-screen flex flex-col items-center justify-center bg-primary-light"></div>
+	);
+};
+
+export default Hero;
